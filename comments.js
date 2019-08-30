535540db3388638d72e149b794e2df5f87e62ad9
antonelagagro <54676848+antonelagagro@users.noreply.github.com>
2019-08-30 14:27:15 +0200

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comments')
+const Topic = require('../models/topics');
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+var checkAuth = require('../middleware/check-auth');
+
+
+//svi komentari
+router.get('/', checkAuth, async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        return res.status(200).json(comments);
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
+
+//komentari za poslani id teme
+router.get('/:id', checkAuth, async (req, res) => {
+    try {
+        var collection = await Comment.find();
+
+        var list = Array();
+        collection.forEach(element => {
+
+            if (element.topicID == req.params.id) {
+                list.push(element);
+            }
+        });
+        return res.status(200).send(list);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+
+//kreiranje komentara
+router.post('/', checkAuth, async (req, res) => {
+    var id = req.userData.userId;
+    try {
+        var topic = Topic.findById(req.body.topicID);
+
+    } catch (error) {
+        return res.json({ message: 'Tema ne postoji!' });
+    }
+
+    if (topic !== null) {
+        const nComment = new Comment({
+            text: req.body.text,
+            topicID: req.body.topicID,
+            userID: id
+        })
+        try {
+            const newComment = await nComment.save();
+            return res.status(201).json(newComment);
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+    else {
+        return res.status(404).json({ message: 'Topic does not exist' });
+    }
+})
+
+//izmjena teksta komentara
+router.patch('/:id', checkAuth, async (req, res) => {
+    var id = req.userData.userId;
+    try {
+        var comment = await Comment.findById(req.params.id);
+    } catch (error) {
+        return res.status(500).json({ message: 'Komentar nije pronađen!' })
+    }
+
+    if (comment.userID != id) {
+        return res.status(403).json({ message: 'Mozete izmijeniti samo svoj komentar!' });
+    }
+    else {
+        if (req.body.text != null) {
+            comment.text = req.body.text;
+        }
+        try {
+            var nComment = await comment.save();
+            return res.status(200).json(nComment);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+})
+
+//brisanje komentara
+router.delete('/:id', checkAuth, async (req, res) => {
+    var id = req.userData.userId;
+    try {
+        var comment = await Comment.findById(req.params.id);
+    } catch (error) {
+        return res.status(400).json({ message: 'Komentar nije pronađen!' });
+    }
+
+
+    if (comment.userID != id) {
+        return res.status(403).json({ message: 'Mozete izbrisati samo svoj komentar!' });
+    }
+    else {
+
+        try {
+            await Comment.findByIdAndDelete(req.params.id)
+            return res.status(200).json({ message: 'Deleted!' })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+
+        }
+    }
+
+})
+
+
+
+
+//pretraga po tekstu u komentarima
+router.get('/text/:text', checkAuth, async (req, res) => {
+    var textT = req.params.text.toLowerCase();
+    Comment.find({
+        $or: [
+            { text: { "$regex": textT, "$options": "i" } }
+        ]
+    }, function (err, data) {
+        if (err)
+            return res.status(500).json({ message: err.message });
+        return res.status(200).json(data);
+    });
+})
+
+module.exports = router;
---
+++ topics.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const router = express.Router();
+const Topic = require('../models/topics')
+const checkAuth = require('../middleware/check-auth')
+
+//paginacija
+router.get('/page/:pageNo', checkAuth, (req, res) => {
+    var pageNo = parseInt(req.params.pageNo)
+    var size = 5;
+    var query = {}
+    if (pageNo < 0 || pageNo === 0) {
+        response = { "error": true, "message": "invalid page number, should start with 1" };
+        return res.status(400).json(response)
+    }
+    query.skip = size * (pageNo - 1)
+    query.limit = size
+    Topic.countDocuments({}, function (err, totalCount) {
+        if (err) {
+            response = { "error": true, "message": "Error fetching data" }
+        }
+        Topic.find({}, {}, query, function (err, data) {
+            if (err) {
+                response = { "error": true, "message": "Error fetching data" };
+            } else {
+                var totalPages = Math.ceil(totalCount / size)
+                response = { "error": false, "message": data, "pages": totalPages };
+            }
+            return res.json(response);
+        });
+    })
+})
+//get sve teme
+router.get('/', checkAuth, async (req, res) => {
+    try {
+        const topics = await Topic.find();
+        return res.status(200).json(topics);
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
+
+//get jedne teme
+router.get('/:id', checkAuth, async (req, res) => {
+    try {
+        topic = await Topic.findById(req.params.id);
+        return res.status(200).send(topic);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+
+
+//kreiranje teme
+router.post('/', checkAuth, async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    var id = req.userData.userId;
+    const nTopic = new Topic({
+        title: req.body.title,
+        text: req.body.text,
+        userID: id
+    })
+    try {
+        const newTopic = await nTopic.save();
+        return res.status(201).json(newTopic);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+
+//izmjena teksta teme
+router.patch('/:id', checkAuth, async (req, res) => {
+    var id = req.userData.userId;
+    try {
+        var topic = await Topic.findById(req.params.id);
+    } catch (error) {
+        return res.status(400).json({ message: 'Tema ne postoji' });
+    }
+    if (topic.userID != id) {
+        return res.status(403).json({ message: 'Mozete izmijeniti samo svoju temu.' });
+    }
+    else {
+
+        if (req.body.text != null) {
+            topic.text = req.body.text;
+        }
+        try {
+            var nTopic = await topic.save();
+            return res.status(200).json(nTopic);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+})
+
+//brisanje teme
+router.delete('/:id', checkAuth, async (req, res) => {
+    var id = req.userData.userId;
+    try {
+
+        var topic = await Topic.findById(req.params.id);
+    } catch (error) {
+        return res.status(400).json({ message: 'Tema ne postoji' });
+    }
+
+
+    if (topic.userID != id) {
+        return res.status(403).json({ message: 'Mozete obrisati samo svoju temu.' })
+    }
+    else {
+        try {
+            await Topic.findByIdAndDelete(req.params.id)
+            return res.status(200).json({ message: 'Deleted!' })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    }
+})
+
+//search po nazivu teme
+router.get('/title/:naziv', checkAuth, async (req, res) => {
+    var titleT = req.params.naziv.toLowerCase();
+
+    Topic.find({
+        $or: [
+            { title: { "$regex": titleT, "$options": "i" } }
+        ]
+    }, function (err, data) {
+        if (err)
+            return res.status(500).json({ message: err.message });
+        return res.status(200).json(data);
+    });
+})
+module.exports = router;
